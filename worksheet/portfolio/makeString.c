2db396653cba4bd36e43e0af7409a8441ba44718
lizzyperry <fzjm0636@leeds.ac.uk>
2026-03-06 00:06:58 +0000

    uisdhyfihs

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Elizabeth Perry
+Student ID: 201952237
 */
 
 #include <stdio.h>
@@ -10,8 +10,19 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[100] = "";
+    int i;
 
     // process the command-line data using appropriate string functions
+    /*
+    The code below will go through the string and will concatenate it to the end, and if i isnt the end of the array then another - will be added
+    */
+    for (i=1; i<argc;i++ ){
+        strcat(buffer, argv[i]);
+        if (i < argc-1){
+            strcat(buffer, "-")
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
